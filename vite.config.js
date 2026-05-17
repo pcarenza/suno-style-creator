d23707595d61990f9f856ba516fd8d8153b6b2f8
pcarenza <pcarenza@gmail.com>
2026-05-17 11:35:25 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/suno-style-creator/', // must match your repo name
 })
